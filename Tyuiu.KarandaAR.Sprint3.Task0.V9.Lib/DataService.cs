f9561c48daa5a87ca376409d75da463e6988661a
KarandaAR <karandaalexandr@gmail.com>
2024-10-30 21:27:24 +0500

    task0

---
+++ Tyuiu.KarandaAR.Sprint3.Task0.V9.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KarandaAR.Sprint3.Task0.V9.Lib
+{
+    public class DataService : ISprint3Task0V9
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sum = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                sum += Math.Pow(1 / (3 + Math.Pow(value, i)), i);
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.KarandaAR.Sprint3.Task0.V9/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KarandaAR.Sprint3.Task0.V9.Lib;
+
+namespace Tyuiu.KarandaAR.Sprint3.Task0.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Каранда А. Р. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Циклы и последовательности                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Каранда Александр Романович | АСОиУб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, используя цикл for, которая вычисляет сумму ряда    *");
+            Console.WriteLine("* по формуле, при x = 0.5.                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            double x = 0.5;
+
+            Console.WriteLine($"Сумма равна: {dataService.GetSumSeries(x, 1, 15)}");
+        }
+    }
+}
