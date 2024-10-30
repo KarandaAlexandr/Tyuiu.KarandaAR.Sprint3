7a5a227309767d8498eefbdebdc8330a8e2e3eaa
KarandaAR <karandaalexandr@gmail.com>
2024-10-30 22:12:19 +0500

    таск 1

--- Tyuiu.KarandaAR.Sprint3.Task1.V9.Lib/DataService.cs
+++ Tyuiu.KarandaAR.Sprint3.Task1.V9.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.KarandaAR.Sprint3.Task1.V9.Lib
             double p = 0;
             for (int k = startValue; k <= stopValue; k++)
             {
-                p += Math.Pow(300 / (Math.Cos(value) + Math.Pow(value, k)), k);
+                p *= Math.Pow(300 / (Math.Cos(value) + Math.Pow(value, k)), k);
             }
             return Math.Round(p, 2);
         }
