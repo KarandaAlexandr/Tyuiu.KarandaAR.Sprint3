8ce7fda87f6c367dd33b6089b649f646cb8433ef
KarandaAR <karandaalexandr@gmail.com>
2024-10-31 23:27:47 +0500

    task 6

---
+++ Tyuiu.KarandaAR.Sprint3.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KarandaAR.Sprint3.Task6.V4.Lib
+{
+    public class DataService : ISprint3Task6V4
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int summa = 0;
+            for (int number = startValue; number <= stopValue; number++)
+            {
+                for (int i = 9; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        summa++;
+                    }
+                }
+            }
+            return summa;
+        }
+    }
+}
---
+++ Tyuiu.KarandaAR.Sprint3.Task6.V4/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KarandaAR.Sprint3.Task6.V4.Lib;
+
+namespace Tyuiu.KarandaAR.Sprint3.Task6.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Каранда А. Р. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование опреаторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Каранда Александр Романович | АСОиУб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [14, 20] количество всех делителей больше 9           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Произведение: {dataService.GetSumTheDivisors(14, 20)}");
+
+            Console.ReadKey();
+        }
+    }
+}
