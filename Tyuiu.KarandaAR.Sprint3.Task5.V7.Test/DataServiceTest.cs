3f013e2c7f2637d010f193264a4d50c57ae90520
KarandaAR <karandaalexandr@gmail.com>
2024-10-31 23:16:21 +0500

    таск 5

---
+++ Tyuiu.KarandaAR.Sprint3.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.KarandaAR.Sprint3.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.KarandaAR.Sprint3.Task5.V7/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.KarandaAR.Sprint3.Task5.V7.Lib;
+
+namespace Tyuiu.KarandaAR.Sprint3.Task5.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Каранда А. Р. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Каранда Александр Романович | АСОиУб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет значение Y по следующей формуле:  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*              3       10                                                 *");
+            Console.WriteLine("*             ---      ---        1                                       *");
+            Console.WriteLine("*      Y =     \\        \\     -----  + x                                *");
+            Console.WriteLine("*             /__ i=1  /__ k=1  cos(k)                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Произведение: {dataService.GetSumSumSeries(5, 1, 3, 1, 10)}");
+
+            Console.ReadKey();
+        }
+    }
+}
