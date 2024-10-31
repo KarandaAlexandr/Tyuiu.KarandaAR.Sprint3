98c2831bf3726a9d6c367627fee403d673a84789
KarandaAR <karandaalexandr@gmail.com>
2024-10-31 22:53:21 +0500

    task 4

---
+++ Tyuiu.KarandaAR.Sprint3.Task4.V20.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KarandaAR.Sprint3.Task4.V20.Lib
+{
+    public class DataService : ISprint3Task4V20
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double p = 1;
+            for (int x = startValue; x < stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    continue;
+                }
+                p *= x / (Math.Cos(x) - x) + 2.5;
+            }
+            return Math.Round(p, 3);
+        }
+    }
+}
---
+++ Tyuiu.KarandaAR.Sprint3.Task4.V20/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.KarandaAR.Sprint3.Task4.V20.Lib;
+
+namespace Tyuiu.KarandaAR.Sprint3.Task4.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Каранда А. Р. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование опреаторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Каранда Александр Романович | АСОиУб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y=x/(cos(x) - x)+2,5                                            *");
+            Console.WriteLine("* При х = 0 пропустить значение. Полученные значения перемножать          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Произведение: {dataService.Calculate(-5, 5)}");
+
+            Console.ReadKey();
+        }
+    }
+}
